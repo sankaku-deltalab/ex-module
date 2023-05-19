38f7806242613ca5e293fe90e29e10eeef2b0894
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-05-19 15:00:01 +0000

    Update Protocol

--- src/core/protocol.ts
+++ src/core/protocol.ts
@@ -1,9 +1,35 @@
 import {ExStruct} from './struct';
 
-export class ExProtocolImpl<Protocol, Impls extends Record<string, Protocol>> {
-  constructor(private readonly impls: Impls) {}
+export interface ExProtocolImpl<
+  Protocol,
+  Implements extends Record<string, Protocol>
+> {
+  of<ModId extends string & keyof Implements>(
+    s: ExStruct<ModId>
+  ): Implements[ModId];
+}
 
-  of<ModId extends string & keyof Impls>(s: ExStruct<ModId>): Impls[ModId] {
+class ExProtocolImplClass<
+  Protocol,
+  Implements extends Record<string, Protocol>
+> {
+  constructor(private readonly impls: Implements) {}
+
+  of<ModId extends string & keyof Implements>(
+    s: ExStruct<ModId>
+  ): Implements[ModId] {
     return this.impls[s.__exStruct__];
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace ExProtocol {
+  export const accumulate = <
+    Protocol,
+    Implements extends Record<string, Protocol>
+  >(
+    impls: Implements
+  ): ExProtocolImpl<Protocol, Implements> => {
+    return new ExProtocolImplClass(impls);
+  };
 }
